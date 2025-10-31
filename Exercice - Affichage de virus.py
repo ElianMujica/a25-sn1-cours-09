139103d32ff019dca5d6b1d0c227d59df59d22c2
ElianMujica <6305353@cegepmontpetit.ca>
2025-10-31 09:01:31 -0400

    exercice 1

---
+++ Exercice - Affichage de virus.py
@@ -0,0 +1,13 @@
+virus = []
+
+
+if len(virus) == 0:
+
+    print("Désolé aucun produit pharmaceutique mortel disponible")
+
+else:
+    for element in virus:
+
+        print(f"Liste des {len(virus)} virus ")
+
+        print(element)
