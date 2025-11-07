239c86b6be8ba63383b8b170e36bb5c35f39bac2
ElianMujica <6305353@cegepmontpetit.ca>
2025-11-06 20:26:26 -0500

    Create Exercice - Manipulation d'ensembles.py

---
+++ Exercice - Manipulation d'ensembles.py
@@ -0,0 +1,11 @@
+ensemble1 = {1, 2, 3, 4, 5}
+ensemble2 = {4, 5, 6, 7, 8}
+
+union = ensemble1 | ensemble2
+print(union)
+
+intersection = ensemble1 & ensemble2
+print(intersection)
+
+difference = ensemble1 - ensemble2
+print(difference)
